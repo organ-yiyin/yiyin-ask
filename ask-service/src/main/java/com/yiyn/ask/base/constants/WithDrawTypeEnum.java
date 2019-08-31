b0ced3fa7b3e60b6957a6f16bf89670beb398e84
lilawen <113382713@qq.com>
2019-08-31 18:29:31 +0100

    src/

---
+++ ask-service/src/main/java/com/yiyn/ask/base/constants/WithDrawTypeEnum.java
@@ -0,0 +1,40 @@
+package com.yiyn.ask.base.constants;
+
+public enum WithDrawTypeEnum {
+	
+	WECHAT("wechat","微信"),
+	OFFLINE("offline","线下");
+	
+	String code;
+	String name;
+	
+	private WithDrawTypeEnum(String code, String name){
+		this.code = code;
+		this.name = name;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public static WithDrawTypeEnum findByCode(Integer code) {
+		for(WithDrawTypeEnum item : WithDrawTypeEnum.values()) {
+			if(item.getCode().equals(code)) {
+				return item;
+			}
+		}
+		return null;
+	}
+}
