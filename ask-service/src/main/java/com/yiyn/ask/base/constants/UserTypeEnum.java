2ae3e5ef5733ea13dff5eddaf9e96867f6cea745
lilawen <113382713@qq.com>
2020-01-18 20:22:44 +0800

    updates

--- ask-service/src/main/java/com/yiyn/ask/base/constants/UserTypeEnum.java
+++ ask-service/src/main/java/com/yiyn/ask/base/constants/UserTypeEnum.java
@@ -1,15 +1,19 @@
 package com.yiyn.ask.base.constants;
 
+import java.math.BigDecimal;
+
 public enum UserTypeEnum {
 	
-	MANAGER(1,"内部"),SERVER(2,"外部");
+	MANAGER(1,"内部", BigDecimal.valueOf(0.3)),SERVER(2,"外部", BigDecimal.valueOf(0.7));
 	
 	Integer code;
 	String name;
+	BigDecimal percent;
 	
-	private UserTypeEnum(Integer code, String name){
+	private UserTypeEnum(Integer code, String name, BigDecimal percent){
 		this.code = code;
 		this.name = name;
+		this.percent = percent;
 	}
 
 	public Integer getCode() {
@@ -26,6 +30,14 @@ public enum UserTypeEnum {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+	
+	public BigDecimal getPercent() {
+		return percent;
+	}
+
+	public void setPercent(BigDecimal percent) {
+		this.percent = percent;
 	}
 
 	public static UserTypeEnum findEnumByCode(Integer code) {
